b731897b1b8a87685dd357ad2695869308ec7b14
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-11 20:35:26 -0300

    Criado entidade Student.

---
+++ src/main/java/entities/Student.java
@@ -0,0 +1,20 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class Student {
+
+    private int id;
+    private String name;
+    private Date dateOfBirth;
+    private String enrollment;
+
+}
