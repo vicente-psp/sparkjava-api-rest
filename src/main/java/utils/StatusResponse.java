8d64d560a48485f358c1f4f32fec47506571eda2
Vicente Silva <vicente.pspenha@gmail.com>
2019-05-12 00:44:28 -0300

    Criado enumerador para status da resposta.

---
+++ src/main/java/utils/StatusResponse.java
@@ -0,0 +1,18 @@
+package utils;
+
+public enum StatusResponse {
+
+    SUCCESS ("Success"),
+    ERROR ("Error");
+
+    final private String status;
+
+    StatusResponse(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+}
